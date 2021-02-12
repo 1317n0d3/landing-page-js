ac80df8a1bb11c52af43bffc59f62783d4b81b5c
klltx <kostya.svetashov.01@gmail.com>
2021-02-12 21:40:52 +0400

    triangle and string tasks

---
+++ js/compareStrings.js
@@ -0,0 +1,5 @@
+function compareStrings(){
+    var str1 = this.string1.value;
+    var str2 = this.string2.value;
+    console.log(str1.length == str2.length);
+}
---
+++ js/triangle.js
@@ -0,0 +1,6 @@
+function getArea (){
+    var height = this.height.value;
+    var base = this.base.value;
+    var area = (height * base) / 2;
+    console.log(area);
+}
