1d1cda5a6d738e6b6acd1bb57beaafc4e466230c
klltx <kostya.svetashov.01@gmail.com>
2021-02-26 12:58:55 +0400

    feat: validation

--- js/compareStrings.js
+++ js/compareStrings.js
@@ -1,5 +1,8 @@
 function compareStrings(){
     let str1 = this.string1.value;
     let str2 = this.string2.value;
-    document.getElementById('task-string-answer').textContent = 'Ответ: ' + (str1.length == str2.length);
+    if(str1.length == 0 ||str2.length == 0)
+        document.getElementById('task-string-answer').textContent = 'Ошибка! Строки не должны быть пустыми.';
+    else
+        document.getElementById('task-string-answer').textContent = 'Ответ: ' + (str1.length == str2.length);
 }
--- js/registration.js
+++ js/registration.js
@@ -1,5 +1,10 @@
 function registration(){
     let login = document.getElementById('login').value;
-    localStorage.setItem('login', login);
-    document.getElementById('sign-up').textContent = localStorage.getItem('login');
+    if(login.length >= 4 && login.length <= 12){
+        localStorage.setItem('login', login);
+        document.getElementById('sign-up').textContent = localStorage.getItem('login');
+    } else {
+        document.getElementById('login').value = '';
+        document.getElementById('login').placeholder = 'Логин должен содержать от 4 до 12 символов!';
+    }
 }
