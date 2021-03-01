e2dd5f2a42319ca2aaeb0105bda1a961f1fa0cb9
klltx <kostya.svetashov.01@gmail.com>
2021-03-01 11:00:16 +0400

    feat: add questions to quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,40 +1,260 @@
 const DATA = [
     {
-        question: 'Вопрос 1',
+        question: 'Какая переменная записана неверно?',
         answers: [
             {
                 id: '1',
-                value: 'Ответ 1',
-                correct: true
+                value: 'var b = false;',
+                correct: false
             },
             {
                 id: '2',
-                value: 'Ответ 2',
+                value: 'var isDone = 0;',
                 correct: false
             },
             {
                 id: '3',
-                value: 'Ответ 3',
+                value: 'var num = "STRING";',
+                correct: false
+            },
+            {
+                id: '4',
+                value: 'var number = 12,5;',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Где верно указан вывод данных?',
+        answers: [
+            {
+                id: '5',
+                value: 'console.log("Hello");',
+                correct: true
+            },
+            {
+                id: '6',
+                value: 'documentWrite("Hello");',
+                correct: false
+            },
+            {
+                id: '7',
+                value: 'print(Hello);',
+                correct: false
+            },
+            {
+                id: '8',
+                value: 'prompt("Hello")',
+                correct: false
+            },
+            {
+                id: '9',
+                value: 'write("Hello");',
                 correct: false
             }
         ]
     },
     {
-        question: 'Вопрос 2',
+        question: 'Где верно указано имя переменной?',
         answers: [
             {
-                id: '4',
-                value: 'Ответ 4',
-                correct: false
-            },
-            {
-                id: '5',
-                value: 'Ответ 5',
+                id: '10',
+                value: 'var num_1;',
                 correct: true
             },
             {
-                id: '6',
-                value: 'Ответ 6',
+                id: '11',
+                value: 'var num',
+                correct: false
+            },
+            {
+                id: '12',
+                value: 'var 1num;',
+                correct: false
+            },
+            {
+                id: '13',
+                value: 'var num-1;',
+                correct: false
+            },
+            {
+                id: '14',
+                value: 'ver num;',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Какие функции выполняет JS?',
+        answers: [
+            {
+                id: '15',
+                value: 'Создает разметку на странице сайта',
+                correct: false
+            },
+            {
+                id: '16',
+                value: 'Создает стилевое оформление сайта',
+                correct: false
+            },
+            {
+                id: '17',
+                value: 'Отвечает за функции на стороне клиента',
+                correct: true
+            },
+            {
+                id: '18',
+                value: 'Выполняет работу с сервером',
+                correct: false
+            },
+            {
+                id: '19',
+                value: 'Отвечает за работу с базами данных',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Где можно использовать JavaScript?',
+        answers: [
+            {
+                id: '20',
+                value: 'Прикладное программное обеспечение',
+                correct: false
+            },
+            {
+                id: '21',
+                value: 'Мобильные приложения',
+                correct: false
+            },
+            {
+                id: '22',
+                value: 'Серверные приложения',
+                correct: false
+            },
+            {
+                id: '23',
+                value: 'Веб-приложения',
+                correct: false
+            },
+            {
+                id: '24',
+                value: 'Можно во всех перечисленных',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Какие циклы есть в языке JavaScript?',
+        answers: [
+            {
+                id: '25',
+                value: 'for, while, do while, foreach',
+                correct: false
+            },
+            {
+                id: '26',
+                value: 'for, forMap, foreach, while',
+                correct: false
+            },
+            {
+                id: '27',
+                value: 'for, while, do while',
+                correct: true
+            },
+            {
+                id: '28',
+                value: 'for, forMap, foreach, while, do while',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Где верно указан запуск всплывающего окна?',
+        answers: [
+            {
+                id: '29',
+                value: 'info ("Hi")',
+                correct: false
+            },
+            {
+                id: '30',
+                value: 'alert ("Hi")',
+                correct: true
+            },
+            {
+                id: '31',
+                value: 'new alert ("Hi")',
+                correct: false
+            },
+            {
+                id: '32',
+                value: 'Нет верных вариантов',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'window[undefined] === undefined?',
+        answers: [
+            {
+                id: '33',
+                value: 'true',
+                correct: true
+            },
+            {
+                id: '34',
+                value: 'false',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'console.log(new Number(1) === 1);',
+        answers: [
+            {
+                id: '35',
+                value: 'false',
+                correct: true
+            },
+            {
+                id: '36',
+                value: 'true',
+                correct: false
+            },
+            {
+                id: '37',
+                value: 'undefined',
+                correct: false
+            },
+            {
+                id: '38',
+                value: 'null',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'with (function(x, undefined){}) length;',
+        answers: [
+            {
+                id: '39',
+                value: '1',
+                correct: false
+            },
+            {
+                id: '40',
+                value: '2',
+                correct: true
+            },
+            {
+                id: '41',
+                value: 'undefined',
+                correct: false
+            },
+            {
+                id: '42',
+                value: 'Error',
                 correct: false
             }
         ]
