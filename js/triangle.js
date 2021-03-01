08f3925bb5ed6aa4662af9b663afc7e0a8492048
klltx <kostya.svetashov.01@gmail.com>
2021-03-01 18:25:45 +0400

    fix: triangle values

--- js/triangle.js
+++ js/triangle.js
@@ -1,6 +1,10 @@
 function getArea (){
     let height = this.height.value;
     let base = this.base.value;
-    let area = (height * base) / 2;
-    document.getElementById('task-triangle-answer').textContent = 'Ответ: ' + area;
+    if(height < 0 || base < 0){
+        document.getElementById('task-triangle-answer').textContent = 'Параметры треугольника не должны быть отрицательными';
+    } else {
+        let area = (height * base) / 2;
+        document.getElementById('task-triangle-answer').textContent = 'Ответ: ' + area;
+    }
 }
